a4b7053c6c7b7e8403e05f7eaaadff217b5abd83
billdevmaster71 <billdevmaster@gmail.com>
2023-08-15 22:17:34 +0900

    fix: remove warnings

--- src/patterns/card.jsx
+++ src/patterns/card.jsx
@@ -192,24 +192,7 @@ const Card = (props) => {
     const from = account
     // return console.log("handle bid", id, atomic(amount, decimals).toString())
     // console.log("amount", atomic(amount, decimals).toString())
-    let maxFeePerGas = ethers.BigNumber.from(40000000000) // fallback to 40 gwei
-    let maxPriorityFeePerGas = ethers.BigNumber.from(40000000000) // fallback to 40 gwei
-    try {
-      const { data } = await axios({
-        method: 'get',
-        url: 'https://gasstation-mainnet.matic.network/v2'
-      })
-      maxFeePerGas = ethers.utils.parseUnits(
-        Math.ceil(data.fast.maxFee) + '',
-        'gwei'
-      )
-      maxPriorityFeePerGas = ethers.utils.parseUnits(
-        Math.ceil(data.fast.maxPriorityFee) + '',
-        'gwei'
-      )
-    } catch {
-      // ignore
-    }
+    
     const gasLimit = 1000000
     if (currency) {
       const tx = chainId === 137 ? await Bidify
